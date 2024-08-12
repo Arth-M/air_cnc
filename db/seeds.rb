f2b1eceb6d5ede923a5213176bb0499de1f2b464
Arthur-Henri Michalland <ah.michalland@gmail.com>
2024-08-12 15:10:20 +0200

    rtu (ready to use)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'destroy db tables'
+User_review.destroy_all
+Flat_review.destroy_all
+Booking.destroy_all
+User.destroy_all
+Flat.destroy_all
+
+puts 'enjoy making the seed'
