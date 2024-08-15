f5fa47d90af2529afadcb4ab5e11041716287187
Andjiane Raveloson <andrianina_raveloson@hotmail.fr>
2024-08-15 14:53:18 +0200

    geocoding started
    Signed-off-by: Andjiane Raveloson <andrianina_raveloson@hotmail.fr>

--- Gemfile
+++ Gemfile
@@ -78,4 +78,8 @@ end
 gem 'faker'
 gem "devise"
 gem 'cloudinary'
+<<<<<<< HEAD
 gem "pg_search"
+=======
+gem "geocoder"
+>>>>>>> f29c061 (geocoding started)
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -12,4 +12,6 @@ class Flat < ApplicationRecord
   using: {
     tsearch: { prefix: true }
   }
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
