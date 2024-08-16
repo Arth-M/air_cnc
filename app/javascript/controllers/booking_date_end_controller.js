236ba1db7d16eb2560618023f162916bd6089e46
Andjiane Raveloson <andrianina_raveloson@hotmail.fr>
2024-08-16 13:16:49 +0200

    Datepicker range + display format in new form
    Signed-off-by: Andjiane Raveloson <andrianina_raveloson@hotmail.fr>

--- app/javascript/controllers/booking_date_end_controller.js
+++ app/javascript/controllers/booking_date_end_controller.js
@@ -3,13 +3,5 @@ import flatpickr from "flatpickr"
 
 // Connects to data-controller="booking-date"
 export default class extends Controller {
-  connect() {
-    flatpickr("#booking_end_date", {
-      dateFormat: "d-m-Y",
-      minDate: new Date().fp_incr(1),
-      "locale": {
-        "firstDayOfWeek": 1 // start week on Monday
-        }
-    })
-  }
+
 }
